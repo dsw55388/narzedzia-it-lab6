aeea9926da576d70b2752dce97902682638ef41e
Tomasz <dsw55388@student.dsw.edu.pl>
2024-07-13 14:33:28 +0200

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,10 +1,39 @@
 import sys
+import json
+import yaml
+import xmltodict
 
 
 def read_file(file_path):
 
     with open(file_path, 'r') as file:
         return file.read()
+
+
+def write_file(file_path, data):
+
+    with open(file_path, 'w') as file:
+        file.write(data)
+
+def convert_data(input_data, input_format, output_format):
+
+    if input_format == 'json':
+        data = json.loads(input_data)
+    elif input_format in ['yml', 'yaml']:
+        data = yaml.safe_load(input_data)
+    elif input_format == 'xml':
+        data = xmltodict.parse(input_data)
+    else:
+        raise ValueError(f"Unsupported input format: {input_format}")
+    
+    if output_format == 'json':
+        return json.dumps(data, indent=4)
+    elif output_format in ['yml', 'yaml']:
+        return yaml.dump(data, sort_keys=False)
+    elif output_format == 'xml':
+        return xmltodict.unparse(data, pretty=True)
+    else:
+        raise ValueError(f"Unsupported output format: {output_format}")
 
 
 def get_file_extension(file_path):
@@ -25,3 +54,6 @@ if __name__ == "__main__":
     output_format = get_file_extension(output_path)
 
     input_data = read_file(input_path)
+    output_data = convert_data(input_data, input_format, output_format)
+    write_file(output_path, output_data)
+    print(f"Converted {input_path} ({input_format}) to {output_path} ({output_format}) successfully.")
