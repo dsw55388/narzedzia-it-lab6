e5def086f2ce70835c8161754de3459cd36d9b22
Tomasz <dsw55388@student.dsw.edu.pl>
2024-07-13 14:25:52 +0200

    Create lab6.py

---
+++ lab6.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def get_file_extension(file_path):
+    return file_path.split('.')[-1].lower()
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("Usage: lab6.exe pathFile1.x pathFile2.y")
+        sys.exit(1)
+
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+    
+    input_format = get_file_extension(input_path)
+    output_format = get_file_extension(output_path)
